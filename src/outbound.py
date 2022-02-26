fb6617bf552208ce74936d1193207b7167313b76
Landon Johnson <landon.johnson@utexas.edu>
2022-02-26 08:37:04 -0600

    tested logging in sendMessage

--- src/outbound.py
+++ src/outbound.py
@@ -1,9 +1,11 @@
 import requests
 import apiserver
+import logging
 
 def sendChat(message):
     body = {
         "bot_id" : apiserver.bot_id,
         "text" : message,
     }
+    #logging.info(body);
     response = requests.post("https://api.groupme.com/v3/bots/post", json=body)
